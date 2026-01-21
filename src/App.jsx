90ad2b91538d7d91fce36d7de62b4a4f058c4b98
Sktanisha <sanjidatanisha99@gmail.com>
2026-01-20 22:32:59 -0500

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
 const App = () => {
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+]);
   return (
-    <div>
-      
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
