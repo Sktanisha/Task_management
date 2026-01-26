94c5e0d1e4ff4debbcb9991b38c2eb4bf34e0d26
Sktanisha <sanjidatanisha99@gmail.com>
2026-01-26 14:26:11 -0500

    signup password icon

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router'
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const Signup = () => {
     let [details, setDetails]= useState({
         email: "",
         password:""
     })
+
+    let [showPassword, setShowPassword]= useState(false)
 
     let handleChange = (e) =>{
         setDetails({
@@ -94,12 +98,21 @@ const Signup = () => {
             <input
             onChange={handleChange}
               name="password"
-              type="password"
+              type={showPassword?"text" : "password"}
               required=""
               className="w-full text-slate-900 text-sm border-b border-slate-300 focus:border-blue-600 pl-2 pr-8 py-3 outline-none"
               placeholder="Enter password"
             />
-            <svg
+            {
+              showPassword?
+              <FaEye onClick={()=>setShowPassword(!showPassword)} className="w-[18px] h-[18px] absolute right-2 cursor-pointer"/>
+              :
+               <FaEyeSlash  onClick={()=>setShowPassword(!showPassword)} className="w-[18px] h-[18px] absolute right-2 cursor-pointer"/>
+            }
+            
+           
+            {/* <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="#bbb"
               stroke="#bbb"
@@ -110,7 +123,7 @@ const Signup = () => {
                 d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
                 data-original="#000000"
               />
-            </svg>
+            </svg> */}
           </div>
         </div>
         <div className="flex flex-wrap items-center justify-between gap-4 mt-8">
